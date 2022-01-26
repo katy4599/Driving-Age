5842f9caf660a6db2cf2c0c1d612393f541e8f5e
katy millard <katy.millard@gmail.com>
2022-01-26 10:13:17 -0600

    Second commit

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const drivingAge = 18;
 const myAge = 41;
 
-    if (myAge >= drivingAge) {
-        console.log('You can drive!');
+if (myAge >= drivingAge) {
+    console.log('You can drive!');
+}
 
-    }else {
-        console.log('You can not drive.');
+else {
+    console.log('You cannot drive.');
 }
